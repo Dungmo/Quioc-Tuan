33c5b4070d6bdc9c7206570713279b9ac56b5817
Dungmo <79953782+Dungmo@users.noreply.github.com>
2022-09-21 14:27:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+function btn1on()
+{
+    document.getElementById("nutnhan").style.background = "#00FF00"
+}
+
+function btn1off()
+{
+    document.getElementById("nutnhan").style.background = "#808080"
+}
+
+function btn2on()
+{
+    document.getElementById("nutnhan2").style.background = "#00FF00"
+}
+
+function btn2off()
+{
+    document.getElementById("nutnhan2").style.background = "#808080"
+}
+
+
+
